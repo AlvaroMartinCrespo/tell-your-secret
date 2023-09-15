574d72dbb856fcd54f67ba4c4fb24b0b922faa65
Alvaro <alvaro.martin.crespo.00@gmail.com>
2023-09-15 11:03:34 +0200

    footer created

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,40 @@
+import { Popover } from '@rewind-ui/core';
 export default function Footer() {
+  const rrss = [
+    {
+      name: 'GitHub',
+      link: 'https://github.com/AlvaroMartinCrespo',
+      img: 'https://cdn-icons-png.flaticon.com/512/25/25231.png',
+    },
+    {
+      name: 'LinkedIn',
+      link: 'https://www.linkedin.com/in/%C3%A1lvaro-mart%C3%ADn-crespo-bb9aa5246/',
+      img: 'https://static.vecteezy.com/system/resources/previews/023/986/926/original/linkedin-logo-linkedin-logo-transparent-linkedin-icon-transparent-free-free-png.png',
+    },
+    {
+      name: 'AlvaroDev',
+      link: 'https://alvarodev.vercel.app/',
+      img: 'https://cdn-icons-png.flaticon.com/512/1089/1089292.png',
+    },
+  ];
   return (
     <>
       <section>
-        <div className="flex justify-center my-[1rem]">
-          <p>Conóceme:</p>
-        </div>
         <div className="flex justify-center gap-[2rem] my-[1rem]">
-          <p>Link GitHub</p>
-          <p>Link LinkedIn</p>
-          <p>Link Pagina Personal</p>
+          {rrss.map((rrss) => (
+            <span key={rrss.name} className="w-10">
+              <Popover>
+                <Popover.Trigger>
+                  <button>
+                    <a href={rrss['link']}>
+                      <img src={rrss['img']} alt={rrss['name']} />
+                    </a>
+                  </button>
+                </Popover.Trigger>
+                <Popover.Content>{rrss['name']}</Popover.Content>
+              </Popover>
+            </span>
+          ))}
         </div>
       </section>
     </>
